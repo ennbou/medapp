5d153b09ef2ff039e88b20a5c6d60042c5773c1f
SE-moustakim <moustakim.salah98@gmail.com>
2020-04-23 22:16:11 +0000

    dao commit

---
+++ src/main/java/com/glsid/medapp/dao/PatientRepository.java
@@ -0,0 +1,15 @@
+package com.glsid.medapp.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.glsid.medapp.enteties.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long>{
+
+	@Query("SELECT p FROM Patient p WHERE p.nom LIKE %:motCle%")
+	public Page<Patient> listPatient(@Param("motCle")String motCle, Pageable pageable);
+}
---
+++ src/main/java/com/glsid/medapp/dao/SecretaireRepository.java
@@ -0,0 +1,9 @@
+package com.glsid.medapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.glsid.medapp.enteties.Secretaire;
+
+public interface SecretaireRepository extends JpaRepository<Secretaire, Long> {
+
+}
