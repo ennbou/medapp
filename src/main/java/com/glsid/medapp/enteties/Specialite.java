d2bf7168a9e800ef23c2712bfbfda172e6011550
SE-moustakim <moustakim.salah98@gmail.com>
2020-04-24 02:31:49 +0000

    .

---
+++ src/main/java/com/glsid/medapp/enteties/Specialite.java
@@ -0,0 +1,90 @@
+package com.glsid.medapp.enteties;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Specialite {
+
+	@Id @GeneratedValue
+	private Long id;
+	private String nom;
+	@OneToMany(mappedBy = "specialite")
+	private Collection<RendezVous> listRendezVous;
+	
+	@OneToMany(mappedBy = "specialite")
+	private Collection<Medecin> listMedecins;
+	
+	
+	
+	public Specialite() {
+		super();
+	}
+
+
+
+	public Specialite(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public Collection<RendezVous> getListRendezVous() {
+		return listRendezVous;
+	}
+
+
+
+	public void setListRendezVous(Collection<RendezVous> listRendezVous) {
+		this.listRendezVous = listRendezVous;
+	}
+
+
+
+	public Collection<Medecin> getListMedecins() {
+		return listMedecins;
+	}
+
+
+
+	public void setListMedecins(Collection<Medecin> listMedecins) {
+		this.listMedecins = listMedecins;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
