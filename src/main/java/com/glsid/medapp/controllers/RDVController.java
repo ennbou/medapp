5e70673af144728c261fe34e3ffc5fbd7df76a05
ennbou <ennbou@yandex.com>
2020-04-24 01:59:36 +0100

    .

---
+++ src/main/java/com/glsid/medapp/controllers/RDVController.java
@@ -0,0 +1,20 @@
+package com.glsid.medapp.controllers;
+
+import com.glsid.medapp.temp.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rdv")
+public class RDVController {
+
+    @RequestMapping(path = {"/", "/liste"})
+    public String liste(Model model) {
+        model.addAttribute("rdvs", Data.listeRDV);
+        return "rdv/liste";
+    }
+
+
+
+}
