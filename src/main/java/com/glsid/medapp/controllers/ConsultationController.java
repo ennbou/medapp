0709deed762d3e170e5c5a2c38bbdff6d4c5125d
MAAZAZ <zakitche@gmail.com>
2020-05-30 03:30:43 +0000

    master

--- src/main/java/com/glsid/medapp/controllers/ConsultationController.java
+++ src/main/java/com/glsid/medapp/controllers/ConsultationController.java
@@ -1,28 +1,63 @@
 package com.glsid.medapp.controllers;
 import java.util.List;
+
+import com.glsid.medapp.dao.ConsultationRepository;
 import com.glsid.medapp.entities.Consultation;
 import com.glsid.medapp.temp.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/consult")
 public class ConsultationController {
 	
-	@RequestMapping(path ="/{id}")
-    public String liste(Model model,@PathVariable int id) {
-		List<Consultation> data=Data2.listeConsult;
-        model.addAttribute("consult", data.get(id));
+	@Autowired
+	private ConsultationRepository consultationController;
+	
+	@GetMapping({"/",""})
+    public String All(Model model) {
+		List<Consultation> consults = consultationController.findAll();
+		// just example 
+		consults.add(Data2.consult);
+        model.addAttribute("consults", consults);
+        return "consult/list";
+    }
+	
+	@GetMapping("/{id}")
+    public String one(Model model,@PathVariable Long id) {
+		// not implemented yet
+        model.addAttribute("consult",Data2.consult);
         return "consult/consultById";
     }
 	
-	@RequestMapping(path ="/")
-    public String liste(Model model) {
-		List<Consultation> data=Data2.listeConsult;
-        model.addAttribute("consult", data);
+	@PostMapping("/{id}")
+	public String save(@RequestBody Consultation consult,@PathVariable Long id) {
+		consult.setId(id);
+		consultationController.save(consult);
         return "consult/list";
     }
-
+	
+	// the same thing
+	@PutMapping("/{id}")
+	public String update(@RequestBody Consultation consultation,@PathVariable Long id) {
+		consultation.setId(id);
+		consultationController.save(consultation);
+		return "consult/list";
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id) {
+		consultationController.deleteById(id);
+		return "consult/list";
+	}
+	
 }
--- src/main/java/com/glsid/medapp/temp/Data2.java
+++ src/main/java/com/glsid/medapp/temp/Data2.java
@@ -2,18 +2,16 @@ package com.glsid.medapp.temp;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import com.glsid.medapp.entities.Consultation;
 
 final public class Data2 {
 
-    public static List<Consultation> listeConsult = new ArrayList<>();
+    public static Consultation consult;
     
     static {
-    	
-    	listeConsult.add(new Consultation(new Long(1),LocalDate.of(2028, 3, 30),LocalTime.of(12,30),LocalTime.of(12,45),"ASPRO + POMADA SAFRA",null,null,null));
+    	consult= new Consultation(new Long(1),LocalDate.of(2028, 3, 30),LocalTime.of(12,30),LocalTime.of(12,45),"ASPRO + POMADA SAFRA",null,null,null);
     }
 
     
