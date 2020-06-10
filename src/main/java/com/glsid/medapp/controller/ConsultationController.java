8950ffbe6c7777b6d216346aa6dd7e78cbd9a4eb
MAAZAZ <zakitche@gmail.com>
2020-06-10 20:18:42 +0100

    update

--- src/main/java/com/glsid/medapp/controller/ConsultationController.java
+++ src/main/java/com/glsid/medapp/controller/ConsultationController.java
@@ -2,6 +2,8 @@ package com.glsid.medapp.controller;
 
 import com.glsid.medapp.dao.ConsultationRepository;
 import com.glsid.medapp.modele.Consultation;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.validation.Valid;
 
@@ -26,7 +28,7 @@ public class ConsultationController {
 	
 	// get all consultations
 	@GetMapping({"","/","/liste"})
-    public String All(Model model,@RequestParam(name="search",defaultValue="") String search,
+    public String All(Model model, @RequestParam(name="search",defaultValue="") String search,
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "size", defaultValue = "10") int size) {
 		//Page<Consultation> consults = consultationRepository.findAll(PageRequest.of(page, size));
@@ -72,8 +74,49 @@ public class ConsultationController {
 		return "redirect:"+"?page="+page+"&size="+size+"&search="+search;
 	}
 	
+	// get consultations by date and search
+	@GetMapping("/recherche")
+	public String searchDate(Model model, @RequestParam(name="search",defaultValue="") String search,
+			LocalDate d1, LocalDate d2,
+	        @RequestParam(name = "page", defaultValue = "0") int page,
+	        @RequestParam(name = "size", defaultValue = "10") int size) {
+		d1= LocalDate.now();
+		d2= LocalDate.now();
+		Page<Consultation> consults = consultationRepository.searchUsingDate(search, d1, d2, PageRequest.of(page, size));
+	    int[] pages = new int[consults.getTotalPages()];
+	    model.addAttribute("pages", pages);
+	    model.addAttribute("size", size);
+	    model.addAttribute("consults", consults);
+	    model.addAttribute("pageActual", page);
+	    model.addAttribute("date1", d1);
+	    model.addAttribute("date2", d2);
+	    return "consult/recherche";
+	}
+	
+	// button search
+	@PostMapping("/recherche/")
+	public String searchDateAvancee(Model model, @RequestParam(name = "search",defaultValue="") String search,
+			@RequestParam(name = "date1") String d1, 
+			@RequestParam(name = "date2") String d2,
+	        @RequestParam(name = "page", defaultValue = "0") int page,
+	        @RequestParam(name = "size", defaultValue = "10") int size) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate date1 = LocalDate.parse(d1, formatter);
+		LocalDate date2 = LocalDate.parse(d2, formatter);
+		Page<Consultation> consults = consultationRepository.searchUsingDate(search, date1, date2, PageRequest.of(page, size));
+	    int[] pages = new int[consults.getTotalPages()];
+	    model.addAttribute("pages", pages);
+	    model.addAttribute("size", size);
+	    model.addAttribute("consults", consults);
+	    model.addAttribute("pageActual", page);
+	    model.addAttribute("search", search);
+	    model.addAttribute("date1", d1);
+	    model.addAttribute("date2", d2);
+	    return "consult/recherche";
+	}
+	
 	// cas d'erreur
-	@GetMapping(path = "/403")
+	@GetMapping("/403")
 	public String accessDenied() {
 		return "403";
 	}
