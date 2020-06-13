c5d0bfd7e6ed40ea8901be62fb945534456dc9ff
MAAZAZ <zakitche@gmail.com>
2020-06-13 01:30:43 +0100

    fix problem of using pageable with string search & Post method

--- src/main/java/com/glsid/medapp/controller/ConsultationController.java
+++ src/main/java/com/glsid/medapp/controller/ConsultationController.java
@@ -96,20 +96,30 @@ public class ConsultationController {
 	public String searchDateAvancee(Model model, @RequestParam(name = "search",defaultValue="") String search,
 			@RequestParam(name = "date1") String d1, 
 			@RequestParam(name = "date2") String d2,
-	        @RequestParam(name = "page", defaultValue = "0") int page,
-	        @RequestParam(name = "size", defaultValue = "10") int size) {
+	        @RequestParam(name = "page", defaultValue = "0") String page,
+	        @RequestParam(name = "size", defaultValue = "10") String size) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate date1 = LocalDate.parse(d1, formatter);
 		LocalDate date2 = LocalDate.parse(d2, formatter);
+		
+		int pageActual=Integer.parseInt(page);
+		int sizePage=Integer.parseInt(size);
+		
 		//use it to search for all input used by user
-		String mots[] = search.split(" ");
-		int i= new Random().nextInt(mots.length);
-		Page<Consultation> consults = consultationRepository.searchUsingDate(mots[i], date1, date2, PageRequest.of(page, size));
+		Page<Consultation> consults=null;
+		if(!search.equals("") && !search.trim().equals("")) {
+			String mots[] = search.split(" ");
+			int i= new Random().nextInt(mots.length);
+			consults = consultationRepository.searchUsingDate(mots[i], date1, date2, PageRequest.of(pageActual, sizePage));
+		}
+		else {
+			consults = consultationRepository.searchUsingDate(search, date1, date2, PageRequest.of(pageActual, sizePage));
+		}
 	    int[] pages = new int[consults.getTotalPages()];
 	    model.addAttribute("pages", pages);
-	    model.addAttribute("size", size);
+	    model.addAttribute("size", sizePage);
 	    model.addAttribute("consults", consults);
-	    model.addAttribute("pageActual", page);
+	    model.addAttribute("pageActual", pageActual);
 	    model.addAttribute("search", search);
 	    model.addAttribute("date1", d1);
 	    model.addAttribute("date2", d2);
@@ -122,4 +132,5 @@ public class ConsultationController {
 	public String accessDenied() {
 		return "consult/erreur";
 	}
+	
 }
